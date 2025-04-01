f125ec21e679c01128bd3aa36e3643d0b7845f67
Khaldi Mamoune <khmamoune33@gmail.com>
2025-04-02 00:28:59 +0100

    *

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ class User(models.Model):
     user_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     user_type = models.CharField(max_length=255)
